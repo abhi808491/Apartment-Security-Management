3428f2037ff4274119b9b72281c95522328db5bc
nabmoham <nabmoham@inlen8520029639.corp.capgemini.com>
2022-03-27 00:03:08 +0530

    Added By Rizwan

---
+++ src/main/java/com/cg/aps/service/VehicleServiceInt.java
@@ -0,0 +1,26 @@
+package com.cg.aps.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.cg.aps.entity.VehicleEntity;
+@Service
+public interface VehicleServiceInt {
+
+	public long add(VehicleEntity bean);
+	
+	public void update(VehicleEntity bean);
+	
+	public void delete(VehicleEntity bean);
+	
+	public VehicleEntity findByName(String name);
+	
+	public VehicleEntity findByPk(long id);
+	
+	public List<VehicleEntity> search(VehicleEntity bean, long pageNo, int pageSize);
+	
+	public List<VehicleEntity> search(VehicleEntity bean);
+	
+	}
+
